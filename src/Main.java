b177f6c0c795ed57aafada42285a6b1bc5f3c021
khovre <mkhovrov01@gmail.com>
2020-09-09 21:35:47 +0300

    NullPointerException fixed, created array output

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,23 @@ public class Main
     {
         Dog a[] = new Dog[3];
 
+        a[0] = new Dog();
         a[0].setAge(2);
         a[0].setName("Шарик");
 
+        a[1] = new Dog();
         a[1].setAge(12);
         a[1].setName("Кубик");
 
+        a[2] = new Dog();
         a[2].setAge(7);
         a[2].setName("Тетраэдрик");
 
-        System.out.println(a[0].toStringDog());
-
-        System.out.println("Человеческий возраст собаки"+ a[0].ageAsHuman());
+        for (int i = 0; i < 3; i++)
+        {
+            System.out.println(a[i].toStringDog());
+            System.out.println("Человеческий возраст собаки: " + a[i].ageAsHuman());
+            System.out.println();
+        }
     }
 }
