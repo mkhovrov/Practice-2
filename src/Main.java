312c1550aeb6fe2bfa9d0f622013cf77d5b54451
khovre <mkhovrov01@gmail.com>
2020-09-09 20:59:53 +0300

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Dog a[] = new Dog[2];
+
+        a[0].setAge(2);
+        a[0].setName("Шарик");
+
+        a[1].setAge(12);
+        a[1].name = "Кубик";
+
+        a[2].setAge(7);
+        a[2].setName("Тетраэдрик");
+
+        System.out.println(a[0].toStringDog());
+    }
+}
